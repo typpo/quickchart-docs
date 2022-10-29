969d73e064d76f09ccff12c5fd20b4548ad48a85
Ian Webster <ianw_github@ianww.com>
2022-10-28 21:51:59 -0700

    improve code with highlights

--- documentation/components/CodeWithHighlights.tsx
+++ documentation/components/CodeWithHighlights.tsx
@@ -14,14 +14,16 @@ export default function CodeWithHighlights({
   return (
     <div
       style={{
+        backgroundColor: 'var(--ifm-code-background)',
         marginBottom: 'var(--ifm-leading)',
+        padding: '2rem 0.5rem',
         overflowX: 'auto',
         whiteSpace: wrap ? 'normal' : 'pre',
         overflowWrap: 'anywhere',
         textAlign: centered ? 'center' : 'left',
       }}
     >
-      <code>
+      <code style={{ border: 'none', backgroundColor: 'transparent' }}>
         {parts.map((part, i) => {
           if (i % 2 === 0) {
             return part;
